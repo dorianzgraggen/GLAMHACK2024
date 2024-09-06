92d277f182d63f0f0a6951d0dfd351bdeb076ace
Dorian Zgraggen <dorianfx1@gmail.com>
2024-09-06 17:27:17 +0200

    get gps location and put it into a store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,7 @@
 import { writable } from "svelte/store";
 
 export const pictures_index = writable(0);
+
+export const latitude = writable(0);
+export const longitude = writable(0);
+export const gps_accuracy = writable(0);
