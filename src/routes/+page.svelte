a4d4311a1cc8cd4f30817559a823e2956c83bbc2
Ihar Shchetko <ihar.shchetko@itu.int>
2024-09-06 18:18:29 +0200

    dist in steps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { database } from '$lib/data';
-	import { calculateDistance } from '$lib/dist';
+	import { calculateDistance, getSteps } from '$lib/dist';
+	import { latitude, longitude } from '$lib/stores';
 </script>
 
 <!-- This is the landing page where we will have all the spots and the distances to the spots -->
 
 <h1>Spots</h1>
 {#each database as record}
-	 <h2>latitude: {record.latitude}, longitude: {record.longitude}
-		distance: {calculateDistance(47.3326290293885, 7.681612352815078, record.latitude, record.longitude)}
+	 <h2>current latitude: {$latitude}, current longitude: {$longitude} 
+		distance: {getSteps(calculateDistance($latitude, $longitude, record.latitude, record.longitude))}
 	 </h2>
 {/each}
