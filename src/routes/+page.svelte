0c3e5dfe1f6a330a2898df261b1c5eaa2063f7d2
Dorian Zgraggen <dorianfx1@gmail.com>
2024-09-07 14:14:40 +0200

    update banner image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -83,7 +83,7 @@
 <style>
 
 	.banner {
-		background-image: url(https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg);
+		background-image: url(/Martiniplan_Luzern.jpg);
 		background-size: cover;
 		background-position: center;
 		width: 100%;
