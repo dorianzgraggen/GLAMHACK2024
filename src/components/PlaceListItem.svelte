36e2c1a8001e4c3d47ec1c7d46c7ff73f225f0c8
Julian Rodriguez <julian.rodriguez.ugolini@gmail.com>
2024-09-07 10:11:35 +0200

    Pictures in startpage

--- src/components/PlaceListItem.svelte
+++ src/components/PlaceListItem.svelte
@@ -5,7 +5,7 @@
     export let place: Place;
 
     
-    const img = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg";
+    const img = "/img/" + place.images[0];
 
 
     function pin()
